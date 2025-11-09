0e1d89cc70b62d76d17f8c5fdef8f57098440acf
rezzza07 <notfunnyatall0007@gmail.com>
2025-11-09 18:00:18 +0600

    CRUD added

--- index.js
+++ index.js
@@ -25,6 +25,59 @@ app.get('/', (req, res) => {
     res.send('Server is running');
 })
 
+async function run() {
+    try {
+        await client.connect();
+
+        const db = client.db('reza_db');
+        const productsCollection = db.collection('products');
+
+        app.get('/products', async (res, res) => {
+            const cursor = productsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/products/:id', async (req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await productsCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productsCollection.insertOne(newProduct);
+            res.send(result);
+        })
+
+        app.patch('/products/id:', async (req, res) => {
+            const id = req.params.id;
+            const updatedProduct = req.body;
+            const query = { _id: new ObjectId(id) }
+            const update = {
+                $set: updatedProduct
+            }
+            const result = await productsCollection.updateOne(query, update);
+            res.send(result);
+        })
+
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await productsCollection.deleteOne(query);
+            res.send(result);
+        })
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir)
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
