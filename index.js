2dfe3cfa6712039c6e6c66b4358bac638a0f27aa
rezzza07 <notfunnyatall0007@gmail.com>
2025-11-11 08:48:18 +0600

    environment variable

--- index.js
+++ index.js
@@ -1,76 +1,97 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 // middleware
- app.use(cors());
- app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
 
- const uri = "mongodb+srv://artdbUser:BptQiolF7wdSfl9v@cluster0.ilappos.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ilappos.mongodb.net/?appName=Cluster0`;
 
- const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
- app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Smart server is running');
- })
+})
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
 
         const db = client.db('art_db');
-        const artsCollection =db.collection('arts');
+        const artsCollection = db.collection('arts');
+        const usersCollection = db.collection('users');
+
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+
+            const email = req.body.email;
+            const query = { email: email };
+            const existingUser = await usersCollection.findOne(query);
+            if (existingUser) {
+                res.send({ message: 'user already exists.' })
+            }
+            else {
+                const result = await usersCollection.insertOne(newUser);
+                res.send(result);
+            }
+
+        })
 
 
-        app.get('/arts',async(req,res)=>{
+        app.get('/arts', async (req, res) => {
             // const projectFields = {title:1}
             // const cursor =artsCollection.find().sort({}).skip(2).limit(6).project(projectFields);
 
-            // console.log(req.query);
-            // const title = req.query.title;
-            // const query = {}
-            // if(title){
-            //     query.title = title;
-            // }
-            const cursor =artsCollection.find();
+            console.log(req.query)
+            const userEmail = req.query.userEmail;
+            const query = {}
+            if (userEmail) {
+                query.userEmail = userEmail;
+            }
+            const cursor = artsCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         });
 
-        app.get('/arts/:id',async(req,res)=>{
-            const id = req.params.idl
-            const query = {_id:new ObjectId(id)}
+        app.get('/arts/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const query = { _id: new ObjectId(id) }
             const result = await artsCollection.findOne(query);
             res.send(result);
         })
 
-        app.post('/arts',async(req,res)=>{
+        app.post('/arts', async (req, res) => {
             const newArt = req.body;
             const result = await artsCollection.insertOne(newArt);
             res.send(result);
         })
 
-        app.patch('arts/:id',async(req,res)=>{
+        app.patch('/arts/:id', async (req, res) => {
+
             const id = req.params.id;
             const updatedArt = req.body;
-            const query = {_id:new ObjectId(id)}
-            const update = {$set:updatedArt}
-            const result = await artsCollection.updateOne(query,update)
+            const query = { _id: new ObjectId(id) }
+            const update = { $set: updatedArt }
+            const result = await artsCollection.updateOne(query, update)
             res.send(result);
         })
 
-        app.delete('/arts/:id',async(req,res)=>{
+        app.delete('/arts/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id:new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await artsCollection.deleteOne(query);
             res.send(result);
         })
@@ -81,7 +102,7 @@ async function run(){
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
-    finally{
+    finally {
 
     }
 
@@ -90,6 +111,6 @@ async function run(){
 run().catch(console.dir);
 
 
- app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Smart server is running on port: ${port}`);
- })
+})
