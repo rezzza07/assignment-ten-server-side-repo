d932c29b261c3adc2762535b1c57160839aa2d47
rezzza07 <notfunnyatall0007@gmail.com>
2025-11-13 10:56:17 +0600

    Explore Artwork My gallery backend and update artwork

--- index.js
+++ index.js
@@ -2,115 +2,107 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-// middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ilappos.mongodb.net/?appName=Cluster0`;
-
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+// Root route
+app.get('/', (req, res) => {
+  res.send('Smart server is running');
 });
 
-app.get('/', (req, res) => {
-    res.send('Smart server is running');
-})
+// MongoDB connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ilappos.mongodb.net/?appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 async function run() {
-    try {
-        await client.connect();
+  try {
+    // Connect to MongoDB
+    await client.connect();
+    const db = client.db('art_db');
+    const artsCollection = db.collection('arts');
+    const usersCollection = db.collection('users');
 
-        const db = client.db('art_db');
-        const artsCollection = db.collection('arts');
-        const usersCollection = db.collection('users');
+    console.log('Connected to MongoDB successfully!');
 
-        app.post('/users', async (req, res) => {
-            const newUser = req.body;
+    // ----------------- Routes -----------------
 
-            const email = req.body.email;
-            const query = { email: email };
-            const existingUser = await usersCollection.findOne(query);
-            if (existingUser) {
-                res.send({ message: 'user already exists.' })
-            }
-            else {
-                const result = await usersCollection.insertOne(newUser);
-                res.send(result);
-            }
+    // Create user
+    app.post('/users', async (req, res) => {
+      const newUser = req.body;
+      const existingUser = await usersCollection.findOne({ email: newUser.email });
+      if (existingUser) return res.send({ message: 'User already exists.' });
 
-        })
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
 
+    // Get all artworks (optional filter by email)
+    app.get('/arts', async (req, res) => {
+      const email = req.query.email;
+      const query = email ? { email } : {}; // filter if email provided
+      const result = await artsCollection.find(query).toArray();
+      res.send(result);
+    });
 
-        app.get('/arts', async (req, res) => {
-            // const projectFields = {title:1}
-            // const cursor =artsCollection.find().sort({}).skip(2).limit(6).project(projectFields);
+    
+    // Get single art by ID
+    app.get('/arts/:id', async (req, res) => {
+      const objectId = new ObjectId(req.params.id);
+      const result = await artsCollection.findOne({ _id: objectId });
+      res.send({ success: true, result });
+    });
 
-            console.log(req.query)
-            const userEmail = req.query.userEmail;
-            const query = {}
-            if (userEmail) {
-                query.userEmail = userEmail;
-            }
-            const cursor = artsCollection.find(query);
-            const result = await cursor.toArray();
-            res.send(result);
-        });
+    // Add new art
+    app.post('/arts', async (req, res) => {
+      const result = await artsCollection.insertOne(req.body);
+      res.send({ success: true, result });
+    });
 
-        app.get('/arts/:id', async (req, res) => {
-            const id = req.params.id;
+    // Update art
+    app.put('/arts/:id', async (req, res) => {
+      const objectId = new ObjectId(req.params.id);
+      const filter = { _id: objectId };
+      const update = { $set: req.body };
+      const result = await artsCollection.updateOne(filter, update);
+      res.send({ success: true, result });
+    });
 
-            const query = { _id: new ObjectId(id) }
-            const result = await artsCollection.findOne(query);
-            res.send(result);
-        })
+    // Patch art
+    app.patch('/arts/:id', async (req, res) => {
+      const objectId = new ObjectId(req.params.id);
+      const update = { $set: req.body };
+      const result = await artsCollection.updateOne({ _id: objectId }, update);
+      res.send(result);
+    });
 
-        app.post('/arts', async (req, res) => {
-            const newArt = req.body;
-            const result = await artsCollection.insertOne(newArt);
-            res.send(result);
-        })
+    // Delete art
+    app.delete('/arts/:id', async (req, res) => {
+      const objectId = new ObjectId(req.params.id);
+      const result = await artsCollection.deleteOne({ _id: objectId });
+      res.send(result);
+    });
 
-        app.patch('/arts/:id', async (req, res) => {
+    // ------------------------------------------------
 
-            const id = req.params.id;
-            const updatedArt = req.body;
-            const query = { _id: new ObjectId(id) }
-            const update = { $set: updatedArt }
-            const result = await artsCollection.updateOne(query, update)
-            res.send(result);
-        })
-
-        app.delete('/arts/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await artsCollection.deleteOne(query);
-            res.send(result);
-        })
-
-
-
-
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    }
-    finally {
-
-    }
-
+  } finally {
+    // do not close client; server will keep running
+  }
 }
 
-run().catch(console.dir);
-
-
-app.listen(port, () => {
-    console.log(`Smart server is running on port: ${port}`);
-})
+// Start server **after connecting to MongoDB**
+run().then(() => {
+  app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+  });
+}).catch(console.dir);
